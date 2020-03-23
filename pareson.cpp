41e3ac2becde8e3636fbda551746174830c60d56
nikolozka <nikoloz_ka@yahoo.com>
2020-03-23 01:42:21 +0000

    Ambisonics mixed in

--- pareson.cpp
+++ pareson.cpp
@@ -3,7 +3,7 @@
 #include "portaudio.h"
 #include "resonance_audio_api.h"
 #include "binaural_surround_renderer.h"
-
+#include "room_effects_utils.cc"
 #include "RTIMULib.h"
 #include "/usr/local/include/oscpack/osc/OscOutboundPacketStream.h"
 #include "/usr/local/include/oscpack/ip/UdpSocket.h"
@@ -45,6 +45,7 @@ static paTestData data;
 int err;
 unsigned int i;
 
+std::size_t quad = 4;
 std::size_t stereo = 2;
 std::size_t mono = 1;
 
@@ -58,20 +59,24 @@ int ebd_r;
 int birb1;
 int birb2;
 int birb3;
+int ambi;
 
 float* ebd_l_buff;
 float* ebd_r_buff;
 float* birb1_buff;
 float* birb2_buff;
 float* birb3_buff;
+float* ambi_buff;
 
 float* ebd_l_p;
 float* ebd_r_p;
 float* birb1_p;
 float* birb2_p;
 float* birb3_p;
+float* ambi_p;
 
-float* out_buffer_ptr;
+float* src_buffer_ptr;
+float* amb_buffer_ptr;
 
 int seekoffset = 1;
 int blockcounter = 0;
@@ -79,6 +84,7 @@ bool readnext = false;
 int bufferoffset = 0;
 
 ResonanceAudioApi* reson;
+BinauralSurroundRenderer* renderer;
 
 RenderingMode quality = kBinauralHighQuality;
 
@@ -87,7 +93,7 @@ SNDFILE* ebd_r_file;
 SNDFILE* birb1_file;
 SNDFILE* birb2_file;
 SNDFILE* birb3_file;
-
+SNDFILE* ambi_file;
 
 static int patestCallback( const void *inputBuffer, void *outputBuffer,
                            unsigned long framesPerBuffer,
@@ -100,6 +106,8 @@ static int patestCallback( const void *inputBuffer, void *outputBuffer,
         reson->SetInterleavedBuffer(birb1,birb1_buff, mono, num_frames);
    	reson->SetInterleavedBuffer(birb2,birb2_buff, mono, num_frames);
    	reson->SetInterleavedBuffer(birb3,birb3_buff, mono, num_frames);
+	renderer->AddInterleavedInput(ambi_buff,  quad, num_frames);
+
 	if(blockcounter == 1 ){
 		readnext=true;
 		bufferoffset = 1;
@@ -115,6 +123,7 @@ static int patestCallback( const void *inputBuffer, void *outputBuffer,
 		birb1_buff=birb1_p;
 		birb2_buff=birb2_p;
 		birb3_buff=birb3_p;
+		ambi_buff=ambi_p;
 	}
 	else{
 		ebd_l_buff+=num_frames;
@@ -122,16 +131,23 @@ static int patestCallback( const void *inputBuffer, void *outputBuffer,
 		birb1_buff+=num_frames;
 		birb2_buff+=num_frames;
 		birb3_buff+=num_frames;
+		ambi_buff+=4*num_frames;
 	}
         blockcounter++;
-  	reson->FillInterleavedOutputBuffer(stereo,num_frames,out_buffer_ptr);
+  	reson->FillInterleavedOutputBuffer(stereo,num_frames,src_buffer_ptr);
+	renderer->GetInterleavedStereoOutput(amb_buffer_ptr,num_frames);
 
     	float *out = (float*)outputBuffer;
     	unsigned int i;
     	(void) inputBuffer; /* Prevent unused variable warning. */
     	for( i=0; i<framesPerBuffer*2; i++ ){
-        	*out++ = out_buffer_ptr[i++];  /* left */
-        	*out++ = out_buffer_ptr[i];  /* right */
+//        	*out++ = amb_buffer_ptr[i++]/2.0;  /* left */
+//       	*out++ = amb_buffer_ptr[i]/2.0;/* right */
+//        	*out++ = src_buffer_ptr[i++]/2.0;  /* left */
+//       	*out++ = src_buffer_ptr[i]/2.0;/* right */
+
+        	*out++ = src_buffer_ptr[i]/1.5 + amb_buffer_ptr[i++]/1.5;  /* left */
+        	*out++ = src_buffer_ptr[i]/1.5  + amb_buffer_ptr[i]/1.5;/* right */
     	}
     	return 0;
 }
@@ -143,10 +159,31 @@ int main(int argc, char** argv) {
 	const char * fbirb1= "resources/birb1.wav" ;
 	const char * fbirb2= "resources/birb2.wav" ;
 	const char * fbirb3= "resources/birb3.wav" ;
+	const char * fambi= "resources/rain.wav" ;
+
+	int sampleCount = 0;
+    	int sampleRate = 0;
+
+  	uint64_t rateTimer;
+    	uint64_t displayTimer;
+    	uint64_t now;
+
+	UdpTransmitSocket transmitSocket( IpEndpointName( ADDRESS, PORT ) );
+    	char buffer[OUTPUT_BUFFER_SIZE];
+    	Quaternion<float> q;
+
+	RTIMUSettings *settings = new RTIMUSettings("RTIMULib");
+    	RTIMU *imu = RTIMU::createIMU(settings);
+
+	RoomProperties* properties = new RoomProperties();
+	ReflectionProperties* refp = new ReflectionProperties();
+	ReverbProperties* revp = new ReverbProperties();
+
         PaError err;
 	PaStream *stream;
 
-  	out_buffer_ptr = (float*) malloc( sizeof(float) * num_frames*2 );
+  	src_buffer_ptr = (float*) malloc( sizeof(float) * num_frames*2 );
+  	amb_buffer_ptr = (float*) malloc( sizeof(float) * num_frames*2 );
 
   	ebd_l_buff = (float*) malloc( sizeof(float) * BUFFER_LEN*2 );
   	ebd_r_buff = (float*) malloc( sizeof(float) * BUFFER_LEN*2 );
@@ -154,11 +191,14 @@ int main(int argc, char** argv) {
   	birb2_buff = (float*) malloc( sizeof(float) * BUFFER_LEN*2 );
   	birb3_buff = (float*) malloc( sizeof(float) * BUFFER_LEN*2 );
 
+  	ambi_buff = (float*) malloc( sizeof(float) * BUFFER_LEN*8 );
+
 	ebd_l_p = ebd_l_buff;
 	ebd_r_p = ebd_r_buff;
 	birb1_p = birb1_buff;
 	birb2_p = birb2_buff;
 	birb3_p = birb3_buff;
+	ambi_p = ambi_buff;
 
 	SF_INFO* info = new SF_INFO();
 
@@ -168,12 +208,20 @@ int main(int argc, char** argv) {
 	birb2_file = sf_open(fbirb2,SFM_READ,info) ;
 	birb3_file = sf_open(fbirb3,SFM_READ,info) ;
 
+	SF_INFO* quadinfo = new SF_INFO();
+
+	ambi_file  = sf_open(fambi,   SFM_READ,quadinfo) ;
+	std::cout<<quadinfo->format<<"\n";
+	std::cout<<quadinfo->channels<<"\n";
+	std::cout<<quadinfo->samplerate<<"\n";
+
 	sf_read_float(ebd_l_file, ebd_l_buff, BUFFER_LEN) ;
 	sf_read_float(ebd_r_file, ebd_r_buff, BUFFER_LEN) ;
 	sf_read_float(birb1_file, birb1_buff, BUFFER_LEN) ;
 	sf_read_float(birb2_file, birb2_buff, BUFFER_LEN) ;
 	sf_read_float(birb3_file, birb3_buff, BUFFER_LEN) ;
 
+	sf_readf_float(ambi_file,  ambi_buff,  BUFFER_LEN) ;
 
 	err=Pa_Initialize();
 	if( err != paNoError )
@@ -195,21 +243,31 @@ int main(int argc, char** argv) {
         if( err != paNoError ) goto error;
   	reson = CreateResonanceAudioApi(stereo,num_frames,sample_rate_hz);
 
-	ReflectionProperties* properties = new ReflectionProperties();
+	renderer = renderer->Create(num_frames,sample_rate_hz,renderer->kFirstOrderAmbisonics);
 
-	properties->room_dimensions[0] = 10.0;
-	properties->room_dimensions[1] = 10.0;
-	properties->room_dimensions[2] = 10.0;
+	properties->dimensions[0] = 10.0;
+	properties->dimensions[1] = 10.0;
+	properties->dimensions[2] = 10.0;
 
-	properties->coefficients[0] = 5;
-	properties->coefficients[1] = 5;
-	properties->coefficients[2] = 5;
-	properties->coefficients[3] = 5;
-	properties->coefficients[4] = 5;
-	properties->coefficients[5] = 5;
+	properties->material_names[0] = kMarble; //left
+	properties->material_names[1] = kMarble; //right
+	properties->material_names[2] = kMarble; //bottom
+	properties->material_names[3] = kMarble; //top
+	properties->material_names[4] = kMarble; //front
+	properties->material_names[5] = kMarble; //back
 
-	reson->SetReflectionProperties(*properties);
+	properties->reflection_scalar = 0.5;
+	properties->reverb_gain = 0.5;
+	properties->reverb_time = 5.0;
+	properties->reverb_brightness = 3.0;
 
+	*refp = ComputeReflectionProperties(*properties);
+	*revp = ComputeReverbProperties(*properties);
+
+	reson->SetReflectionProperties(*refp);
+	//reson->SetReverbProperties(*revp); //causes stutter?
+
+	reson->EnableRoomEffects(true);
   	reson->SetMasterVolume(1.0);
   	reson->SetHeadPosition(0,1,0);
 
@@ -219,21 +277,19 @@ int main(int argc, char** argv) {
   	birb2 = reson->CreateSoundObjectSource(quality);
   	birb3 = reson->CreateSoundObjectSource(quality);
 
-  	reson->SetSourcePosition(ebd_l,1,1,-0.5);
-  	reson->SetSourcePosition(ebd_r,-1,1,-0.5);
-  	reson->SetSourcePosition(birb1,-2,1,-3);
-  	reson->SetSourcePosition(birb2,2,1,4);
-  	reson->SetSourcePosition(birb3,0,1,-4);
 
-	int sampleCount = 0;
-    	int sampleRate = 0;
+  	reson->SetSourcePosition(ebd_l,1,1,-2.5);
+  	reson->SetSourcePosition(ebd_r,-1,1,-2.5);
+  	reson->SetSourcePosition(birb1,-2,1,-2);
+  	reson->SetSourcePosition(birb2,2,1,3);
+  	reson->SetSourcePosition(birb3,0,1,-3);
 
-  	uint64_t rateTimer;
-    	uint64_t displayTimer;
-    	uint64_t now;
+	reson->SetSourceRoomEffectsGain(ebd_l,1);
+	reson->SetSourceRoomEffectsGain(ebd_r,1);
+	reson->SetSourceRoomEffectsGain(birb1,1);
+	reson->SetSourceRoomEffectsGain(birb2,1);
+	reson->SetSourceRoomEffectsGain(birb3,1);
 
-	RTIMUSettings *settings = new RTIMUSettings("RTIMULib");
-    	RTIMU *imu = RTIMU::createIMU(settings);
     	if ((imu == NULL) || (imu->IMUType() == RTIMU_TYPE_NULL)) {
         	std::cout<<"No IMU found\n";
 		goto error;
@@ -248,10 +304,6 @@ int main(int argc, char** argv) {
 
     	rateTimer = RTMath::currentUSecsSinceEpoch();
 
-	UdpTransmitSocket transmitSocket( IpEndpointName( ADDRESS, PORT ) );
-
-    	char buffer[OUTPUT_BUFFER_SIZE];
-    	Quaternion<float> q;
 
 
 	err = Pa_StartStream( stream );
@@ -279,7 +331,7 @@ int main(int argc, char** argv) {
            }
         }
 
-        q.Print();
+        //q.Print();
 
         osc::OutboundPacketStream p( buffer, OUTPUT_BUFFER_SIZE );
         p << osc::BeginBundleImmediate
@@ -308,6 +360,14 @@ int main(int argc, char** argv) {
         transmitSocket.Send( p.Data(), p.Size() );
 
 	reson->SetHeadRotation(q.Getx(),q.Gety(),q.Getz(),q.Gets());
+	renderer->SetHeadRotation(q.Gets(),q.Getx(),q.Gety(),q.Getz());
+
+	if(sf_seek(ebd_l_file,1,SEEK_CUR)==-1) sf_seek(ebd_l_file,0,SEEK_SET); else  sf_seek(ebd_l_file,-1,SEEK_CUR);
+	if(sf_seek(ebd_r_file,1,SEEK_CUR)==-1) sf_seek(ebd_r_file,0,SEEK_SET); else  sf_seek(ebd_r_file,-1,SEEK_CUR);
+	if(sf_seek(birb1_file,1,SEEK_CUR)==-1) sf_seek(birb1_file,0,SEEK_SET); else  sf_seek(birb1_file,-1,SEEK_CUR);
+	if(sf_seek(birb2_file,1,SEEK_CUR)==-1) sf_seek(birb2_file,0,SEEK_SET); else  sf_seek(birb2_file,-1,SEEK_CUR);
+	if(sf_seek(birb3_file,1,SEEK_CUR)==-1) sf_seek(birb3_file,0,SEEK_SET); else  sf_seek(birb3_file,-1,SEEK_CUR);
+	if(sf_seek(ambi_file,1,SEEK_CUR)==-1) sf_seek(ambi_file,0,SEEK_SET);   else  sf_seek(ambi_file,-1,SEEK_CUR);
 
 	if(readnext){
 		readnext = false;
@@ -316,8 +376,11 @@ int main(int argc, char** argv) {
 		sf_read_float(birb1_file, birb1_p+bufferoffset*BUFFER_LEN, BUFFER_LEN) ;
 		sf_read_float(birb2_file, birb2_p+bufferoffset*BUFFER_LEN, BUFFER_LEN) ;
 		sf_read_float(birb3_file, birb3_p+bufferoffset*BUFFER_LEN, BUFFER_LEN) ;
+		sf_readf_float(ambi_file,  ambi_p+bufferoffset*BUFFER_LEN*4,  BUFFER_LEN) ;
 		}
 	}
+	
+
 	//Pa_Sleep(NUM_SECONDS*1000);
 
 	err = Pa_StopStream( stream );
@@ -330,13 +393,16 @@ int main(int argc, char** argv) {
 	sf_close(birb1_file);
 	sf_close(birb2_file);
 	sf_close(birb3_file);
+	sf_close(ambi_file);
 
-	free(out_buffer_ptr);
+	free(src_buffer_ptr);
+	free(amb_buffer_ptr);
   	free(ebd_l_buff);
   	free(ebd_r_buff);
   	free(birb1_buff);
   	free(birb2_buff);
   	free(birb3_buff);
+  	free(ambi_buff);
   	return 0;
 
   error:
@@ -349,13 +415,18 @@ int main(int argc, char** argv) {
 	sf_close(birb1_file);
 	sf_close(birb2_file);
 	sf_close(birb3_file);
+	sf_close(ambi_file);
 
-	free(out_buffer_ptr);
+	free(src_buffer_ptr);
+	free(amb_buffer_ptr);
+
   	free(ebd_l_buff);
   	free(ebd_r_buff);
   	free(birb1_buff);
   	free(birb2_buff);
   	free(birb3_buff);
+  	free(ambi_buff);
+
   	fprintf( stderr, "Error number: %d\n", err );
   	fprintf( stderr, "Error message: %s\n", Pa_GetErrorText( err ) );
   	return err;
